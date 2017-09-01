4aeed0dd93d02e3fd0a3f485857507444d36e308
Aslak Knutsen <aslak@4fs.no>
2017-09-01 18:38:29 +0200

    Initial version
    Logs all POST requests to StdOut in JSON format

---
+++ main_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRawOutput(t *testing.T) {
+	log, status := run(t, "text/plain", "a-b-c")
+	assert.Equal(t, http.StatusOK, status)
+
+	assert.Contains(t, log, "\"msg\":\"a-b-c\"")
+	assert.Contains(t, log, "test-ns")
+	assert.Contains(t, log, "test-ot")
+	assert.Contains(t, log, "test-on")
+}
+
+func TestJSONOutput(t *testing.T) {
+	log, status := run(t, "application/json", "{\"a\":1}")
+	assert.Equal(t, http.StatusOK, status)
+
+	assert.Contains(t, log, "\"json\":{\"a\":1}")
+	assert.Contains(t, log, "test-ns")
+	assert.Contains(t, log, "test-ot")
+	assert.Contains(t, log, "test-on")
+}
+
+func TestBadJSONOutput(t *testing.T) {
+	log, status := run(t, "application/json", "{\"a:1}")
+	assert.Equal(t, http.StatusBadRequest, status)
+
+	assert.Contains(t, log, "\"msg\":\"{\\\"a:1}")
+	assert.Contains(t, log, "test-ns")
+	assert.Contains(t, log, "test-ot")
+	assert.Contains(t, log, "test-on")
+}
+
+func run(t *testing.T, contenttype, content string) (string, int) {
+	buf := bytes.NewBuffer([]byte{})
+
+	format := new(log.JSONFormatter)
+	format.TimestampFormat = "2006-01-02 15:04:05"
+
+	logger := &log.Logger{
+		Out:       buf,
+		Formatter: format,
+		Hooks:     make(log.LevelHooks),
+		Level:     log.InfoLevel,
+	}
+
+	r := CreateRouter(logger)
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	url := ts.URL + "/log/test-ns/test-ot/test-on"
+	resp, err := http.Post(url, contenttype, bytes.NewBufferString(content))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return buf.String(), resp.StatusCode
+}
